PixelExperience/build

build: Add our props to ADDITIONAL_BUILD_PROPERTIES
Change-Id: I81f55f3a0a91e79eca4dcd22cf8f10354fff7b30

core/main.mk
@@ -128,6 +128,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add our custom properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(CUSTOM_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk

adeelashfaqhad21/revengeOS

core/main.mk
@@ -175,6 +175,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add our custom properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(REVENGEOS_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk